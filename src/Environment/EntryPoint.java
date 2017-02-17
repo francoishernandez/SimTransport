1ab550f28a8f432741b5fbed2ef08396c539a17f
FH <francois.hernandez.fh@gmail.com>
2017-02-17 09:41:40 +0100

    points

---
+++ src/Environment/EntryPoint.java
@@ -0,0 +1,15 @@
+package Environment;
+
+import Environment.Point;
+
+public class EntryPoint extends Point {
+
+	private String name;
+	
+	public EntryPoint(String name, int x, int y, int z) {
+		super(x, y, z);
+		this.name = name;
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/Environment/InterestPoint.java
@@ -0,0 +1,13 @@
+package Environment;
+
+public class InterestPoint extends Point {
+
+	private String name;
+	
+	public InterestPoint(String name, int x, int y, int z) {
+		super(x, y, z);
+		this.name = name;
+		// TODO Auto-generated constructor stub
+	}
+
+}

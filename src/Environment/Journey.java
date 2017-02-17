9e1acb2fa9a0b5925d5e942f8725bc2346d726ce
FH <francois.hernandez.fh@gmail.com>
2017-02-17 18:19:59 +0100

    journey

---
+++ src/Environment/Journey.java
@@ -0,0 +1,42 @@
+package Environment;
+
+import java.util.ArrayList;
+
+// Décrit le trajet d'un point A à un point B
+
+public class Journey {
+
+	private Point A;
+	private Point B;
+	private ArrayList<Path> paths;
+	
+	public Journey(Point A, Point B){
+		this.A = A;
+		this.B = B;
+	}
+	
+	public void setA(Point p){
+		this.A = p;
+	}
+	
+	public void setB(Point p){
+		this.B = p;
+	}
+	
+	public Point getA(){
+		return A;
+	}
+	
+	public Point getB(){
+		return B;
+	}
+	
+	public void addPath(Path p){
+		this.paths.add(p);
+	}
+	
+	public ArrayList<Path> getPaths(){
+		return paths;
+	}
+	
+}

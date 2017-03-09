e0d64985290633c6425c3a288624b42f9ceb2560
FH <francois.hernandez.fh@gmail.com>
2017-03-09 17:44:30 +0100

    parser RER/Bus/Foot

--- src/Util/Import.java
+++ src/Util/Import.java
@@ -16,7 +16,7 @@ import Environment.Paths.*;
 // et ils sont récupérables à l'aide des différents getters.
 
 public class Import {
-	
+
 	ArrayList<Point> points = new ArrayList<Point>();
 	ArrayList<Path> userPaths = new ArrayList<Path>();
 	ArrayList<Path> carPaths = new ArrayList<Path>();
@@ -25,7 +25,7 @@ public class Import {
 
 	// Hashmap des points créés à l'importation, pour faciliter la création des chemins (appel des objets correspondants)
 	private Map<String, Point> pointsMap = new HashMap<String, Point>();
-	
+
 	public Import() throws IOException {
 		pointsEntree = getEntryPoints();
 		points.addAll(pointsEntree);
@@ -35,11 +35,13 @@ public class Import {
 		points.addAll(getPointsRER());
 		userPaths.addAll(getFootPaths());
 		userPaths.addAll(getBikePaths());
+		userPaths.addAll(getBusPaths());
+		userPaths.addAll(getRerPaths());
 		carPaths.addAll(getRoadPaths());
 		carPaths.addAll(getHighwayPaths());
 	};
-	
-	
+
+
 	// Importe les points d'intérêt
 	public ArrayList<InterestPoint> getInterestPoints() throws IOException {
 		ArrayList<InterestPoint> points = new ArrayList<InterestPoint>();
@@ -66,13 +68,13 @@ public class Import {
 			pointsMap.put(pointID, point);
 			lines--;
 		}
-		
+
 		br.close();
 
 		return points;
 	}
-	
-	
+
+
 	// Importe les points d'entrée
 	public ArrayList<EntryPoint> getEntryPoints() throws IOException {
 		ArrayList<EntryPoint> points = new ArrayList<EntryPoint>();
@@ -99,12 +101,12 @@ public class Import {
 			pointsMap.put(pointID, point);
 			lines--;
 		}
-		
+
 		br.close();
 
 		return points;
 	}
-	
+
 	// Importe les points 'de passage'
 	public ArrayList<Point> getPoints() throws IOException {
 		ArrayList<Point> points = new ArrayList<Point>();
@@ -131,12 +133,12 @@ public class Import {
 			pointsMap.put(pointID, point);
 			lines--;
 		}
-		
+
 		br.close();
 
 		return points;
 	}
-	
+
 	// Importe les points de RER
 	public ArrayList<Point> getPointsRER() throws IOException {
 		ArrayList<Point> points = new ArrayList<Point>();
@@ -163,12 +165,12 @@ public class Import {
 			pointsMap.put(pointID, point);
 			lines--;
 		}
-		
+
 		br.close();
 
 		return points;
 	}
-	
+
 	// Importe les chemins autoroute
 	public ArrayList<HighwayPath> getHighwayPaths() throws IOException {
 		ArrayList<HighwayPath> paths = new ArrayList<HighwayPath>();
@@ -193,12 +195,12 @@ public class Import {
 			paths.add(pathReturn);
 			lines--;
 		}
-		
+
 		br.close();
 
 		return paths;
 	}
-	
+
 	// Importe les chemins route
 	public ArrayList<RoadPath> getRoadPaths() throws IOException {
 		ArrayList<RoadPath> paths = new ArrayList<RoadPath>();
@@ -223,12 +225,12 @@ public class Import {
 			paths.add(pathReturn);
 			lines--;
 		}
-		
+
 		br.close();
 
 		return paths;
 	}
-	
+
 	// Importe les chemins piétons
 	public ArrayList<Path> getFootPaths() throws IOException {
 		ArrayList<Path> paths = new ArrayList<Path>();
@@ -254,72 +256,159 @@ public class Import {
 			paths.add(pathReturn);
 			lines--;
 		}
-		
+
 		br.close();
-		
+
+
+		filer = new FileReader("objects/roadPaths.csv");
+		// NOMBRE DE LIGNES
+		lines = 0;
+		reader = new BufferedReader(filer);
+		while(reader.readLine()!=null) lines++;
+		reader.close();
+		// LECTURE DU FICHIER
+		fr = new FileReader("objects/roadPaths.csv");
+		br = new BufferedReader(fr);
+		br.readLine(); // éliminer la ligne d'entête
+		while (lines>1) {
+			String[] line = br.readLine().split(",");
+			String pointAID = line[0];
+			String pointBID = line[1];
+			FootPath path = new FootPath(pointsMap.get(pointAID), pointsMap.get(pointBID));
+			//System.out.println(path.toString());
+			FootPath pathReturn = new FootPath(pointsMap.get(pointBID), pointsMap.get(pointAID));
+			paths.add(path);
+			paths.add(pathReturn);
+			lines--;
+		}
 		/*for (Path p: paths){
 			System.out.println(p.toString());
 		}*/
 
 		return paths;
 	}
-	
+
 	// Importe les chemins cyclistes
-		public ArrayList<Path> getBikePaths() throws IOException {
-			ArrayList<Path> paths = new ArrayList<Path>();
-			System.out.println("IN GETBIKEPATH");
-			FileReader filer = new FileReader("objects/bikePaths.csv");
+	public ArrayList<Path> getBikePaths() throws IOException {
+		ArrayList<Path> paths = new ArrayList<Path>();
+		System.out.println("IN GETBIKEPATH");
+		FileReader filer = new FileReader("objects/bikePaths.csv");
+		// NOMBRE DE LIGNES
+		int lines = 0;
+		BufferedReader reader = new BufferedReader(filer);
+		while(reader.readLine()!=null) lines++;
+		reader.close();
+		// LECTURE DU FICHIER
+		FileReader fr = new FileReader("objects/bikePaths.csv");
+		BufferedReader br = new BufferedReader(fr);
+		br.readLine(); // éliminer la ligne d'entête
+		while (lines>1) {
+			String[] line = br.readLine().split(",");
+			String pointAID = line[0];
+			String pointBID = line[1];
+			BikePath path = new BikePath(pointsMap.get(pointAID), pointsMap.get(pointBID));
+			//System.out.println(path.toString());
+			BikePath pathReturn = new BikePath(pointsMap.get(pointBID), pointsMap.get(pointAID));
+			paths.add(path);
+			paths.add(pathReturn);
+			lines--;
+		}
+
+		br.close();
+
+		/*for (Path p: paths){
+				System.out.println(p.toString());
+			}*/
+
+		return paths;
+	}
+
+	// Importe les chemins RER
+	public ArrayList<RerPath> getRerPaths() throws IOException {
+		ArrayList<RerPath> paths = new ArrayList<RerPath>();
+		System.out.println("IN GETRERPATH");
+		FileReader filer = new FileReader("objects/rerPaths.csv");
+		// NOMBRE DE LIGNES
+		int lines = 0;
+		BufferedReader reader = new BufferedReader(filer);
+		while(reader.readLine()!=null) lines++;
+		reader.close();
+		// LECTURE DU FICHIER
+		FileReader fr = new FileReader("objects/rerPaths.csv");
+		BufferedReader br = new BufferedReader(fr);
+		br.readLine(); // éliminer la ligne d'entête
+		while (lines>1) {
+			String[] line = br.readLine().split(",");
+			String pointAID = line[0];
+			String pointBID = line[1];
+			int lineID = Integer.parseInt(line[2]);
+			RerPath path = new RerPath(pointsMap.get(pointAID), pointsMap.get(pointBID), lineID);
+			RerPath pathReturn = new RerPath(pointsMap.get(pointBID), pointsMap.get(pointAID), lineID);
+			paths.add(path);
+			paths.add(pathReturn);
+			lines--;
+		}
+
+		br.close();
+
+		return paths;
+	}
+	
+	// Importe les chemins Bus
+		public ArrayList<BusPath> getBusPaths() throws IOException {
+			ArrayList<BusPath> paths = new ArrayList<BusPath>();
+			System.out.println("IN GETRERPATH");
+			FileReader filer = new FileReader("objects/busPaths.csv");
 			// NOMBRE DE LIGNES
 			int lines = 0;
 			BufferedReader reader = new BufferedReader(filer);
 			while(reader.readLine()!=null) lines++;
 			reader.close();
 			// LECTURE DU FICHIER
-			FileReader fr = new FileReader("objects/bikePaths.csv");
+			FileReader fr = new FileReader("objects/busPaths.csv");
 			BufferedReader br = new BufferedReader(fr);
 			br.readLine(); // éliminer la ligne d'entête
 			while (lines>1) {
 				String[] line = br.readLine().split(",");
 				String pointAID = line[0];
 				String pointBID = line[1];
-				BikePath path = new BikePath(pointsMap.get(pointAID), pointsMap.get(pointBID));
-				//System.out.println(path.toString());
-				BikePath pathReturn = new BikePath(pointsMap.get(pointBID), pointsMap.get(pointAID));
+				int lineID = Integer.parseInt(line[2]);
+				BusPath path = new BusPath(pointsMap.get(pointAID), pointsMap.get(pointBID), lineID);
+				BusPath pathReturn = new BusPath(pointsMap.get(pointBID), pointsMap.get(pointAID), lineID);
 				paths.add(path);
 				paths.add(pathReturn);
 				lines--;
 			}
-			
+
 			br.close();
-			
-			/*for (Path p: paths){
-				System.out.println(p.toString());
-			}*/
 
 			return paths;
 		}
-	
+
+
+
+
 	// GETTERS DES LISTES D'OBJETS
-	
+
 	public ArrayList<EntryPoint> getPointsEntree(){
 		return pointsEntree;
 	}
-	
+
 	public ArrayList<InterestPoint> getPointsInteret(){
 		return pointsInteret;
 	}
-	
+
 	public ArrayList<Point> getAllPoints(){
 		return points;
 	}
-	
+
 	public ArrayList<Path> getUserPaths(){
 		return userPaths;
 	}
-	
+
 	public ArrayList<Path> getCarPaths(){
 		return carPaths;
 	}
-	
+
 
 }
